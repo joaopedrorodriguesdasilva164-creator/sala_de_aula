8564364e5aa8f539791a57277fad44f92500807a
JOAO PEDRO RODRIGUES DA SILVA <87812789004@senacrs.edu.br>
2026-09-17 21:56:45 -0300

    loop

--- exercicios/condicionais.py
+++ exercicios/condicionais.py
@@ -13,3 +13,22 @@ def fizz_buzz(numero:int):
     if__name__=="__main__:"
     teste= fizz_buzz(15)
     print(teste)
+    #Exercício 2
+def verificar_maioridade(idade:int)
+    if idade>18
+    return "maior_de_idade"
+    else:
+    return"menor_de_idade"
+
+
+if__name__=="__main__":
+    teste=fizz_buzz(15)
+print(teste)
+
+#exercício 3
+if numero > 0:
+    return "postivo"
+elif numero < 0:
+     return "negativo"
+
+ 
--- exercicios/funcoes_basico.py
+++ exercicios/funcoes_basico.py
@@ -1,14 +1,11 @@
 def formatar_saudacao(nome:str,cidade:str):
     return f"Olá {nome}, seja bem-vinda(a) a {cidade}!"
 
-if __name__=='__main__':
-    saudacao=formatar_saudacao("Alice","Porto alegre")
-    print(saudacao)
     
-    #exercício 2
-    def calcular_perimetro(largura:float,altura:float)->
-        perimetro=2*(largura+altura)
-    print("EXERCICIOS========================\N\N
+#exercício 2
+def calcular_perimetro(largura:float,altura:float):
+    perimetro=2*(largura+altura)
+    print("EXERCICIOS========================\N\N")
     saudacao=formatar_saudacao("Alice""Porto alegre")
     print(f"1- {saudacao}")
     perimetro= calcular_perimetro(altura=10,largura=5)
@@ -16,20 +13,27 @@ if __name__=='__main__':
 
 #exercicio 3
 def fahrenheit_para_celsius(temp_f: float):
-temp_celsisus= (temp_f- 32)* (5/9)
-return temp_celsius
+    temp_celsisus= (temp_f- 32)* (5/9)
+    return temp_celsisus
 
 #Exercício 4
-def calcular_gorjeta_por_pessoa(conta:float,porcentagem_gorjeta:float,gorjeta=(conta*porcentagem_gorjeta/100)/pessoas
-  return gorjeta
+def calcular_gorjeta_por_pessoa(conta:float,porcentagem_gorjeta:float,pessoas:int):
+    gorjeta=(conta*porcentagem_gorjeta/100)/pessoas
+    return gorjeta
 
-  #Exercicio 5
-  def resumo_circulo(raio:float:) 
-  pi=3.14159
-  area= pi*(raio**2) 
-  return f"um circulo de raio{raio} tem área de {area:.2f}"
+#Exercicio 5
+def resumo_circulo(raio:float): 
+    pi=3.14159
+    area= pi*(raio**2) 
+    return f"um circulo de raio{raio} tem área de {area:.2f}"
 
-  #Exercício 6 
-  def resumo_juros_basico(capital:float,taxa:float)                           
+#Exercício 6 
+def resumo_juros_basico(capital:float,taxa:float):
+    pass
+  
+#Exercício 7
 
-  #Exercício 7
+
+if __name__=='__main__':
+    saudacao=formatar_saudacao("Alice","Porto alegre")
+    print(saudacao)
